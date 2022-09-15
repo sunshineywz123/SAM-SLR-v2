67cbbc697633a4c38274b0c6ddb7728281e88c30
yuanweizhong <yuanweizhong@sensetime.com>
2022-09-15 16:29:34 +0800

    addd gitignore

--- SL-GCN/graph/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import tools
-from . import sign_27
-from . import sign_42
--- SL-GCN/model/__init__.py
+++
@@ -1,4 +0,0 @@
-# from . import decouple_gcn_attn2
-from . import decouple_gcn_attn
-from . import dropSke
-from . import dropT
